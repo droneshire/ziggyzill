8f82a5e68dab44d6a2a787e5e90eb011e57b2935
rossyeager <ross@boostedboards.com>
2019-12-09 01:15:42 -0800

    wip

--- src/scrape_zillow.py
+++ src/scrape_zillow.py
@@ -2,17 +2,97 @@
 
 from lxml import html
 
-import requests
-import unicodecsv as csv
 import argparse
 import json
 import os
+import random
+import requests
+import time
+import unicodecsv as csv
+
+ZILLOW_URL = 'https://www.zillow.com'
+
+def get_response(url, headers, response_path=None):
+    # Getting response from zillow.com.
+    for i in range(5):
+        print('URL: {}'.format(url))
+        response = requests.get(url, headers=headers)
+        if response_path:
+            save_to_file(response_path, response.text)
+        if response.status_code != 200:
+            continue
+        return response
+    return None
 
 def clean(text):
     if text:
         return ' '.join(' '.join(text).split())
     return None
-        
+
+def save_to_file(path, data):
+    with open(path, 'w') as fp:
+        fp.write(data.encode('utf8'))
+
+class ZillowHtmlDownloader(object):
+    """ Class that downloads zillow zip code searches for scraping """
+    
+    @staticmethod
+    def get_headers():
+        # Creating headers.
+        headers = {'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+                'accept-encoding': 'gzip, deflate, sdch, br',
+                'accept-language': 'en-GB,en;q=0.8,en-US;q=0.6,ml;q=0.4',
+                'cache-control': 'max-age=0',
+                'upgrade-insecure-requests': '1',
+                'user-agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.131 Safari/537.36'}
+        return headers
+    
+    def __init__(self, zip_code):
+        self.zip_code = zip_code
+    
+    def create_starting_url(self):
+        # Creating Zillow URL based on the filter.
+        url = os.path.join(ZILLOW_URL, 'homes/for_sale/', self.zip_code)
+        url += '_rb/?fromHomePage=true&shouldFireSellPageImplicitClaimGA=false&fromHomePageTab=buy'
+        return url
+
+    def query_zillow(self):
+        url = self.create_starting_url()
+        response = get_response(url, ZillowHtmlDownloader.get_headers())
+        if not response:
+            print("Failed to fetch the page.")
+            return None
+
+        parser = html.fromstring(response.text)
+        total_homes_results = int(parser.xpath('//div/div/div[@class="search-subtitle"]/span[@class="result-count"]//text()')[0].split()[0])
+        PROPERTIES_PER_PAGE = 40
+        # don't add 1 b/c we've already queried the first page
+        pages_to_query = total_homes_results / PROPERTIES_PER_PAGE
+        next_page = parser.xpath('//li[@class="zsg-pagination-next"]/a/@href')[0]
+        next_page_url = os.path.dirname(next_page.rstrip('/'))
+        next_page_prefix = ZILLOW_URL + next_page_url
+
+        print('Found {} results'.format(total_homes_results))
+        print('Pulling results from page 1/{}'.format(pages_to_query + 1))
+        responses = [response.text]
+        for page in range(2, 2 + pages_to_query):
+            url = os.path.join(next_page_prefix, '{}_p'.format(page))
+            response = get_response(url, self.get_headers())
+            if not response:
+                print("Failed to fetch the next page.")
+                break
+            responses.append(response.text)
+
+            print('Pulling results from page {}/{}'.format(page, pages_to_query + 1))
+            with open('/tmp/foo{}.html'.format(page), 'w') as f:
+                f.write(response.text.encode('utf8'))
+    
+            parser = html.fromstring(response.text)
+            time.sleep(random.random() * 2.0)
+        return responses
+
+    
+    
 class Property(object):
 
     def __init__(self):
@@ -32,7 +112,6 @@ class Property(object):
         self.area = ''
         self.days_on_zillow = ''
 
-ZILLOW_URL = 'https://www.zillow.com/'
 
 class PropertyHtml(Property):
 
@@ -105,22 +184,16 @@ def maybe_get_json_results(parser):
     return properties
 
 
-class ZillowSearchHtml(object):
-    """ Class for managing Zillow search html """    
-    def __init__(self, html_input, description):
-        if os.path.isfile(html_input):
-            with open(html_input) as htmlfile:
-                self.raw_html = htmlfile.read()
-        elif isinstance(html_input, str):
-            self.raw_html = html_input
-        else:
-            raise ValueError
+class ZillowScraper(object):
+    """ Class for scraping Zillow search html """    
 
-        parser = html.fromstring(self.raw_html)
+    def __init__(self, description):
         self.description = description
         self.properties_list = []
-
         self.addresses = []
+    
+    def parse_properties(self, raw_html):
+        parser = html.fromstring(raw_html)
         # try json first as it generally has more consistent info
         properties = maybe_get_json_results(parser)
         # try parsing the xml directly afterwards
@@ -130,9 +203,6 @@ class ZillowSearchHtml(object):
                 print('Found {}'.format(prop.address))
                 self.addresses.append(prop.address)
                 self.properties_list.append(prop)
-        
-    
-    def get_properties(self):
         return self.properties_list
     
     def write_data_to_csv(self):
@@ -140,7 +210,6 @@ class ZillowSearchHtml(object):
         filename = 'properties-{}.csv'.format(self.description)
         print('Saving to {}'.format(filename))
         with open(filename, 'wb') as csvfile:
-            print(fieldnames)
             writer = csv.DictWriter(csvfile, fieldnames=sorted(fieldnames))
             writer.writeheader()
             print('Saving {} properties'.format(len(self.properties_list)))
@@ -152,12 +221,34 @@ class ZillowSearchHtml(object):
     
 def parse_args():
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('filename', help='html file of Zillow search results')
+    parser.add_argument('--html-filename', help='html file of Zillow search results')
+    parser.add_argument('--zip-code', help='zip code to search')
     parser.add_argument('description', help='description')
     return parser.parse_args()
 
 if __name__ == "__main__":
     args = parse_args()
-    zsearch = ZillowSearchHtml(args.filename, args.description)
-    properties = zsearch.get_properties()
+    results_pages = []
+    assert args.zip_code is not None or args.html_filename is not None, 'invalid args'
+
+    if args.zip_code:
+        html_path = 'properties-{}-{}.html'.format(args.description, args.zip_code)
+        zquery = ZillowHtmlDownloader(args.zip_code)
+        results_pages.extend(zquery.query_zillow())
+        if not results_pages:
+            assert args.html_filename, 'Must specify a downloaded html file since we cannot query zillow!'
+            results_pages.extend([args.html_filename])
+    elif args.html_filename:
+        with open(args.html_filename) as htmlfile:
+            raw_html = htmlfile.read()  
+        results_pages.extend([raw_html])
+
+    zsearch = ZillowScraper(args.description)
+    for result in results_pages:
+        try:
+            print('Parsing result...')
+            zsearch.parse_properties(result)
+        except:
+            print(result)
+            raise
     zsearch.write_data_to_csv()
