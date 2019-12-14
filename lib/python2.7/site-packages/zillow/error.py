55a780136a73ac59cb4e89a9e1523752fdadb69f
rossyeager <ross@boostedboards.com>
2019-12-14 01:09:38 -0800

    iwp

--- lib/python2.7/site-packages/zillow/error.py
+++
@@ -1,9 +0,0 @@
-class ZillowError(Exception):
-    """Base class for Zillow errors"""
-
-    @property
-    def message(self):
-        """
-        :return: The first argument used to construct this error.
-        """
-        return self.args[0]
