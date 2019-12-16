26db92993ad80047daec10d8f568226f096b20ce
rossyeager <ross@boostedboards.com>
2019-12-16 00:59:49 -0800

    configs

--- app.py
+++ app.py
@@ -1,10 +1,16 @@
-from flask import Flask
-app = Flask(__name__)
+import flask 
+import os
 
+app = flask.Flask(__name__)
+app.config.from_object(os.environ['APP_SETTINGS'])
 
 @app.route('/')
-def hello():
-    return "Hello World!"
+def ecf():
+    return "Check out www.engineeredcashflow.com"
+
+@app.route('/<zipcode>')
+def ecf_zipcode(zipcode):
+    return "Finding data for {}!".format(zipcode)
 
 if __name__ == '__main__':
     app.run()
---
+++ config.py
@@ -0,0 +1,27 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config(object):
+    DEBUG = False
+    TESTING = False
+    CSRF_ENABLED = True
+    SECRET_KEY = 'bf86cf26-1fe0-11ea-91b8-54a050870797'
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+
+
+class StagingConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class DevelopmentConfig(Config):
+    DEVELOPMENT = True
+    DEBUG = True
+
+
+class TestingConfig(Config):
+    TESTING = True
